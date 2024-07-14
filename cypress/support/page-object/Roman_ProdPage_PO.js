938d34bbfcfdcec46072aa6835f48db8963cacad
Roman4331 <eagle3141592@yahoo.com>
2024-07-13 23:06:03 -0500

    added feature, steps, two POs and fixture files

---
+++ cypress/support/page-object/Roman_ProdPage_PO.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+class Roman_ProdPage_PO {
+  constructor() {
+    // CSS selectors
+    this.addToCartButton = "a[onclick='addToCart(1)']";
+  }
+
+  //Product Page Methods
+  clickAddToCartBtn() {
+    cy.get(this.addToCartButton).click();
+  }
+
+  verifyProdAddedToCart() {
+    cy.on("window:alert", (message) => {
+      expect(message).to.equal("Product added");
+    });
+  }
+  verifyProductDescription() {
+    cy.fixture("Roman_HomePage_PO_fixture.json").then((data) => {
+      cy.get(".name").should(
+        "eq",
+        data.productNames["samsung"]
+      );
+    });
+  }
+}
+export default Roman_ProdPage_PO;
