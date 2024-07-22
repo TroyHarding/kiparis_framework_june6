6b51b47e3c4f2fcb42bd26fd4cf0bc158406f7cb
Susanna Chang <susannachang@gmail.com>
2024-07-22 02:48:19 -0700

    Added modified DemoBlaze Feature and Steps Files

--- cypress/e2e/susannaDemoBlaze.feature
+++ cypress/e2e/susannaDemoBlaze.feature
@@ -1,50 +1,35 @@
 @demo @susanna
 Feature: Demoblaze site
+ 
+ @demo1 @susanna
+ Scenario: User Registration
+    Given I navigate to the Demoblaze homepage
+    When I click on the Sign Up button
+    When I enter username "billyjoebob" and password "password1"
+    When I click on Sign Up button
+    Then I should see a registration confirmation
 
-  @demo1 @susanna
-  Scenario: User Registration
-    Given I open url "https://demoblaze.com/"
-    When I click on element with selector "#signin2"
-    Then element with selector "#signInModalLabel" should be present
-    When I type "susanna321" into element with selector "#sign-username"
-    When I type "passworD" into element with selector "#sign-password"
-    When I click on element with selector "button[onclick='register()']"
+@demo2 @susanna
+Scenario: User Login
+    Given I navigate to the Demoblaze homepage
+    When I click on the Log In button
+    When I enter username "billyjoebob" and password "password1"
+    When I click Log In in the modal
+    Then I verify should be logged in
 
-  @demo2 @susanna
-  Scenario: User Login
-    Given I open url "https://demoblaze.com/"
-    When I click on element with selector "#login2"
-    Then element with selector "div[id='logInModal'] div[class='modal-header']" should be present
-    Then I wait for 2 sec
-    When I type "susanna321" into element with selector "#loginusername"
-    When I type "passworD" into element with selector "#loginpassword"
-    When I click on element with selector "button[onclick='logIn()']"
-    # Then element with selector "a#nameofuser.nav-link" should be present
-    Then element with selector "#nameofuser" should be present
-    Then I wait for 2 sec
+@demo3 @susanna
+Scenario: Add Product to Cart
+    Given I navigate to the Demoblaze homepage
+    When I click a product "Samsung galaxy s6"
+    When I add the product to the cart
+    Then I should see product added to the cart
 
-  @demo3 @susanna
-  Scenario: Add Product to Cart
-    Given I open url "https://demoblaze.com/"
-    When I click on element with selector "body > div:nth-child(6) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1) > a:nth-child(1)"
-    # When I click on element with selector "h4[class='card-title']"
-    Then I wait for 2 sec
-    When I click on element with selector ".btn.btn-success.btn-lg"
-    When I click on element with selector "#cartur"
-    Then I wait for element with selector "#tbodyid" to be present
-
-  @demo4 @susanna
-  Scenario: Place an Order
-    Given I open url "https://demoblaze.com/"
-    When I click on element with selector "body > div:nth-child(6) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > h4:nth-child(1) > a:nth-child(1)"
-    Then I wait for 2 sec
-    When I click on element with selector ".btn.btn-success.btn-lg"
-    When I click on element with selector "#cartur"
-    And  I click on element with selector "body:nth-child(2) div:nth-child(7) div.row div.col-lg-1 > button.btn.btn-success:nth-child(3)"
-    When I type "susanna" into element with selector "#name"
-    When I type "usa" into element with selector "#country"
-    When I type "san francisco" into element with selector "#city"
-    When I type "123456123456789" into element with selector "#card"
-    When I type "07" into element with selector "#month"
-    When I type "2024" into element with selector "#year"
-    When I click on element with selector "button[onclick='purchaseOrder()']"
+@demo4 @susanna
+Scenario: Place an Order
+    Given I navigate to the Demoblaze homepage
+    When I click a product "Samsung galaxy s6"
+    When I add the product to the cart
+    When I click on the Cart button
+    When  I click the Place Order Button
+    When I fill out the Place Order Field
+    Then I should see an order confirmation
--- cypress/support/step_definitions/susannaDemoBlaze_Steps.js
+++ cypress/support/step_definitions/susannaDemoBlaze_Steps.js
@@ -1,77 +1,102 @@
 /// <reference types="cypress" />
-import {
-    Given,
-    When,
-    Then,
-    And,
-  } from "@badeball/cypress-cucumber-preprocessor";
-  import "cypress-iframe";
-  // import demoblaze_PO from "../page-object/demoblaze_PO";
-  
-  // const demo = new demoblaze_PO();
-  
-  //   Given(`I login to Demoblaze as a valid user`, () => {
-  //    demo.loginValid();
-  //   });
-  
-  Given("I navigate to the Demoblaze homepage", () => {
-    cy.visit("https://www.demoblaze.com/");
-  });
-  
-  When("I click on the Log In button", () => {
-    cy.get("#login2").click();
-    cy.wait(1000);
-  });
-  
-  When(
-    "I enter username {string} and password {string}",
-    (username, password) => {
-      cy.get("#loginusername").type(username);
-      cy.wait(1000);
-      cy.get("#loginpassword").type(password);
-    }
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import "cypress-iframe";
+  import susannaDemoBlaze_PO() from "../page-object/susannaDemoBlaze_PO()";
+
+  const demo = new susannaDemoBlaze_PO();
+
+// Sign Up
+Given(`I navigate to the Demoblaze homepage`, () => {
+  cy.visit("https://demoblaze.com/");
+});
+
+When(`I click on the Sign Up button`, () => {
+  cy.get("#signin2").click();
+  cy.get("#signInModalLabel").should("be.visible");
+});
+
+
+When(
+  `I enter username {string} and password {string}`,
+  (username, password) => {
+    cy.get("#sign-username").type(username);
+    cy.get("#sign-password").type(password);
+  }
+);
+
+When(`I click on Sign Up button`, () => {
+  cy.get("button[onclick='register()']").click();
+});
+
+Then(`I should see a registration confirmation`, () => {
+  cy.contains("Sign up successful.").should("be.visible");
+});
+
+// Login
+
+When(`I click on the Log In button`, () => {
+  cy.get("#signin2").click();
+  cy.get("div[id='logInModal'] div[class='modal-header']").contains("Log in");
+  cy.wait(2000);
+});
+
+When(
+  `I enter username {string} and password {string}`,
+  (username, password) => {
+    cy.get("#loginusername").type(username);
+    cy.get("#loginpassword").type(password);
+  }
+);
+
+When(`I click Log In in the modal`, () => {
+  cy.get("button[onclick='logIn()']").click();
+});
+
+Then(`I verify should be logged in`, () => {
+  cy.get("#nameofuser").should("be.visible");
+  cy.wait(2000);
+});
+
+When(`I click a product {string}`, (product) => {
+  cy.get("h4 > [href$='prod.html?idp_=1']").click();
+  cy.get("#tbodyid").contains("Samsung galaxy s6");
+  cy.wait(2000);
+});
+
+When(`I add the product to the cart`, () => {
+  cy.get(".btn.btn-success.btn-lg").click();
+});
+
+Then(`I should see product added to the cart`, () => {
+  cy.contains("Product added").should("be.visible").click();
+});
+
+// Place an Order
+
+When(`I click on the Cart button`, () => {
+  cy.get("#cartur").click();
+  cy.get("#tbodyid").contains("Samsung galaxy s6");
+  cy.wait(2000);
+});
+
+When(`I click the Place Order Button`, () => {
+  cy.get(".btn.btn-success").click();
+  cy.get("div[id='orderModal'] div[class='modal-header']").contains(
+    "Place order"
   );
-  
-  When("I click Log In in the modal", () => {
-    cy.get('.modal-footer .btn.btn-primary[onclick="logIn()"]').click();
-    cy.wait(2000);
-  });
-  Then("I verify should be logged in", () => {
-    cy.get("#nameofuser").should("have.text", "Welcome billyjoebob");
-  });
-  
-  When("I click a product {string}", (product) => {
-    cy.contains(".card-title", product).click();
-    cy.wait(1000);
-  });
-  Then("I add the product to the cart", () => {
-    cy.get('a.btn.btn-success.btn-lg[onclick="addToCart(1)"]').click();
-  });
-  
-  Then("the product should be added to the cart", () => {
-    // the www.demoblaze.com says Product added to the cart doesn't appear in cypress
-  });
-  
-  When("I click on the Cart button", () => {
-  cy.get('#cartur').click()
-  cy.wait(1000)
-  });
-  
-  Then("I click the Place Order Button", () => {
-  cy.get('[data-target="#orderModal"]').click()
-  });
-  
-  When("I fill out the Place Order Field", () => {
-    cy.get('#name').type("billyjoebob")
-  cy.get('#country').type("USA")
-    cy.get('#city').type("Boulder")
-  cy.get('#card').type("4111111111111111")
-  cy.get('#month').type("03")
-    cy.get('#year').type("2028")
-    cy.get('button.btn.btn-primary[type="button"][onclick="purchaseOrder()"]').click()
-  });
-  
-  Then("I should see an order confirmation", () => {
-  cy.wait(1000)
-  cy.contains("Thank you for your purchase!").should('be.visible')
-  });
+  cy.wait(2000);
+});
+
+When("I fill out the Place Order Field", () => {
+  cy.get("#name").type("billyjoebob");
+  cy.get("#country").type("usa");
+  cy.get("#city").type("san francisco");
+  cy.get("#card").type("12345678910");
+  cy.get("#month").type("march");
+  cy.get("#year").type("2024");
+  cy.get("button[onclick='purchaseOrder()']").click();
+});
+
+Then(`I should see an order confirmation`, () => {
+  cy.contains("Thank you for your purchase!").should("be.visible");
+});
